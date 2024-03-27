a6e94f5225c353e6e4f7befea0cbe43898200e3f
franz1007 <franz.dindorf@gmail.com>
2024-03-27 20:43:19 +0100

    working map from tutorial

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,37 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+
+export class MapComponent implements AfterViewInit {
+  private map!: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+  }
+
+  private tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    minZoom: 3,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+    this.tiles.addTo(this.map)
+  }
+}
