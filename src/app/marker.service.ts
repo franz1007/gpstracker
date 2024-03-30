cfb76d6c924e083b996d6f6145d06fa7c91b65a5
franz1007 <franz.dindorf@gmai.com>
2024-03-30 15:28:22 +0100

    Changed sorting of points, added circles with diameter increasing for newer points

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -13,24 +13,27 @@ export class MarkerService {
     this.http.get(this.capitals).subscribe((res: any) => {
       console.log(res)
       const points = []
-      res.sort((a : any, b: any) => {
-        if ( a.timestamp < b.timestamp ){
-          return -1;
-        }
-        if ( a.timestamp > b.timestamp ){
+      res.sort((a: any, b: any) => {
+        if (a.timestamp < b.timestamp) {
           return 1;
+        }
+        if (a.timestamp > b.timestamp) {
+          return -1;
         }
         return 0;
       })
+      let marker = new L.CircleMarker([0, 0])
       for (const c of res) {
         points.push([c.lat, c.lon])
-        const lon = c.lon;
-        const lat = c.lat;
-        const marker = L.circleMarker([lat, lon]);
+      }
+      for (let i = 0; i < points.length && i < 20; i++) {
+        marker = new L.CircleMarker([points[i][0], points[i][1]])
+        marker.setRadius(20-i)
         marker.addTo(map);
       }
+      points.lastIndexOf
       console.log(points)
-      const line = L.polyline(points, {color: "red"}).addTo(map)
+      const line = L.polyline(points, { color: "red" }).addTo(map)
     });
   }
 }
