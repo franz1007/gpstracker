97c8ff6c05e34b1eee8b792fa9f343c3202bef4a
franz1007 <franz.dindorf@gmail.com>
2024-11-17 03:04:42 +0100

    Viewable map courtesy of KVision

--- src/jsMain/kotlin/eu/franz1007/gpstracker/web/Main.kt
+++ src/jsMain/kotlin/eu/franz1007/gpstracker/web/Main.kt
@@ -1,58 +1,30 @@
 package eu.franz1007.gpstracker.web
 
+import io.kvision.MapsModule
+import io.kvision.maps.DefaultTileLayers
+import io.kvision.maps.LeafletObjectFactory
+import io.kvision.maps.externals.leaflet.geo.LatLng
 import kotlinx.browser.document
-import kotlinx.browser.window
-import kotlinx.html.a
 import kotlinx.html.div
 import kotlinx.html.dom.append
 import kotlinx.html.dom.create
-import kotlinx.html.p
-import org.w3c.dom.WebSocket
-import org.w3c.xhr.XMLHttpRequest
+import kotlinx.html.id
+import kotlinx.html.style
 
 fun main() {
+    MapsModule.initialize()
     val body = document.body ?: error("No body")
     body.append {
         div {
-            p {
-                +"Here is "
-                a("https://kotlinlang.org") { +"official Kotlin site" }
-            }
+            id = "testDiv"
         }
     }
-    val timeP = document.create.p {
-        +"Time: 0"
+    val mapDiv = document.create.div {
+        style = "height: 500px; width: 1000px;"
+        id="mapDiv"
     }
-    val pointsP = document.create.p{
-        +"test"
-    }
-
-    body.append(timeP, pointsP)
-
-    val req = XMLHttpRequest()
-    req.onload =  {
-        println(it)
-        pointsP.textContent = req.responseText
-        console.log(req)
-    }
-    req.open("GET", "http://localhost:8090/api/points")
-    req.send()
-
-    val ws = WebSocket("ws://localhost:8090/api/ws")
-    ws.onmessage = {
-        println(it.data)
-        pointsP.textContent += it.data.toString()
-        when (val data = it.data){
-            is String -> pointsP.textContent+=data
-            else -> println("Could not find type")
-        }
-    }
-    var time = 0
-    window.setInterval({
-        time++
-        timeP.textContent = "Test: $time"
-
-        return@setInterval null
-    }, 1000)
-
+    body.append(mapDiv)
+    val map = LeafletObjectFactory.map(mapDiv)
+    DefaultTileLayers.OpenStreetMap.addTo(map)
+    map.setView(LatLng(51.505, -0.09), 13)
 }
