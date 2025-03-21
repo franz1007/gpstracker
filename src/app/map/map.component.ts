dc5f343cc6ca53fd257b74f60a165326bcba8a0b
franz1007 <franz.dindorf@gmai.com>
2025-03-21 01:34:12 +0100

    Working tracking

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,13 +1,15 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MarkerService } from '../marker.service';
+import { SsePointService } from '../ssePoint.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
-    selector: 'app-map',
-    imports: [],
-    templateUrl: './map.component.html',
-    styleUrl: './map.component.css'
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
 })
 
 export class MapComponent implements AfterViewInit {
@@ -18,8 +20,10 @@ export class MapComponent implements AfterViewInit {
     minZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   });
+  private line!: L.Polyline
+  private marker!: L.CircleMarker
 
-  constructor(private markerService: MarkerService) { }
+  constructor(private markerService: MarkerService, private sseService: SsePointService) { }
 
   ngAfterViewInit(): void {
     this.map = L.map('map', {
@@ -27,6 +31,23 @@ export class MapComponent implements AfterViewInit {
       zoom: 10
     });
     this.tiles.addTo(this.map)
-    this.markerService.makeCapitalMarkers(this.map);
+    //this.markerService.makeCapitalMarkers(this.map);
+    this.markerService.getInitialPoints().then(points => {
+      this.line = L.polyline(points, { color: "red" }).addTo(this.map)
+      const pos = this.line.getLatLngs()[this.line.getLatLngs().length - 1] as L.LatLng
+      this.marker = L.circleMarker(pos)
+      this.marker.setRadius(20)
+      this.marker.addTo(this.map)
+      this.sseService.createEventSource().subscribe(data => {
+        console.log(data)
+        const obj = JSON.parse(data)
+        console.log(obj.lat)
+        console.log(obj.lon)
+        this.line.addLatLng(new L.LatLng(obj.lat, obj.lon))
+        this.marker.setLatLng(this.line.getLatLngs()[this.line.getLatLngs().length - 1] as L.LatLng)
+      })
+    })
+    
+    
   }
 }
--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -2,38 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
 import { environment } from '../environments/environment';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
-  pointsUrl: string = environment.apiUrl + "/points"
+  pointsUrl: string = environment.apiUrl + "/api/points"
 
   constructor(private http: HttpClient) { }
-  makeCapitalMarkers(map: L.Map): void {
-    this.http.get(this.pointsUrl).subscribe((res: any) => {
-      console.log(res)
-      const points = []
-      res.sort((a: any, b: any) => {
-        if (a.timestamp < b.timestamp) {
-          return 1;
-        }
-        if (a.timestamp > b.timestamp) {
-          return -1;
-        }
-        return 0;
-      })
-      for (const c of res) {
-        points.push([c.lat, c.lon])
+
+  async getInitialPoints(): Promise<L.LatLng[]> {
+    const res = await firstValueFrom(this.http.get(this.pointsUrl)) as Array<any>
+    const points = Array<L.LatLng>()
+    res.sort((a: any, b: any) => {
+      if (a.timestamp < b.timestamp) {
+        return -1;
       }
-      for (let i = 0; i < points.length && i < 20; i++) {
-        const marker = L.circleMarker([points[i][0], points[i][1]])
-        marker.setRadius(20-i)
-        marker.addTo(map);
+      if (a.timestamp > b.timestamp) {
+        return 1;
       }
-      points.lastIndexOf
-      console.log(points)
-      const line = L.polyline(points, { color: "red" }).addTo(map)
-    });
+      return 0;
+    })
+    for (const c of res) {
+      points.push(new L.LatLng(c.lat, c.lon))
+    }
+    console.log("test123")
+    console.log(res)
+    return points
   }
 }
