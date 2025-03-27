00f77a1176edba5a03013118728e487326c28d2f
franz1007 <franz.dindorf@gmai.com>
2025-03-27 22:07:24 +0100

    Tracks

---
+++ src/commonMain/kotlin/eu/franz1007/gpstracker/model/GpsPointNoId.kt
@@ -0,0 +1,19 @@
+package eu.franz1007.gpstracker.model
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GpsPointNoId(
+    val timestamp: Instant,
+    val lat: Double,
+    val lon: Double,
+    val hdop: Double,
+    val altitude: Double,
+    val speed: Double,
+    val bearing: Double,
+    val eta: Instant,
+    val etfa: Instant,
+    val eda: Int,
+    val edfa: Int
+)
