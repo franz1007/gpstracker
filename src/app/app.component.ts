0b3effa6d8811199eaec12ab4acde34c612aca8e
franz1007 <franz.dindorf@gmai.com>
2025-03-29 17:42:51 +0100

    Save Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
 import { DrawerModule } from 'primeng/drawer'
 import { ButtonModule } from 'primeng/button'
+import { CommonModule } from '@angular/common';
 @Component({
-    selector: 'app-root',
-    imports: [MapComponent, DrawerModule, ButtonModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [MapComponent, DrawerModule, ButtonModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  visible = true
   title = 'angular-leaflet-example';
+  isExpanding = false;
+  toggleSideBar() {
+    this.isExpanding = !this.isExpanding;
+  }
 }
