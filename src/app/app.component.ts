4dcf47219793a9e9822400f63b88eb6286799fa0
franz1007 <franz.dindorf@gmai.com>
2025-03-31 01:23:30 +0200

    binding works with tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, signal, WritableSignal, InputSignal, input, effect } from '@angular/core';
 import { MapComponent } from './map/map.component';
 import { DrawerModule } from 'primeng/drawer'
 import { ButtonModule } from 'primeng/button'
 import { CommonModule } from '@angular/common';
-import { TreeModule } from 'primeng/tree';
+import { TreeModule, TreeNodeSelectEvent } from 'primeng/tree';
 import { TreeNode, } from 'primeng/api';
 import { TrackNoPoints } from './map/trackNoPoints';
 import { TrackService } from './map/services/track.service';
@@ -20,62 +20,68 @@ export class AppComponent {
   title = 'angular-leaflet-example';
   files!: TreeNode[];
   isExpanding = false;
-  mapTrackMode : string|TrackNoPoints= "latest"
+  mapTrackMode: string | TrackNoPoints = "latest"
   private tracks!: Array<TrackNoPoints>
+  selection: WritableSignal<any> = signal("test")
 
-  constructor(private trackService: TrackService){
-
+  constructor(private trackService: TrackService) {
+    effect(() => {
+      const selection = this.selection()
+      if (selection !== undefined && selection !== null) {
+        const data = selection.data
+        console.log("effect")
+        console.log(selection)
+        if (data !== null && data !== undefined) {
+          if (data === 'latest') {
+            this.mapTrackMode = 'latest'
+          }
+          else {
+            if (typeof (data.id) === 'number') {
+              this.mapTrackMode = data
+            }
+          }
+        }
+      }
+    })
   }
 
   toggleSideBar() {
     this.isExpanding = !this.isExpanding;
   }
   ngOnInit() {
-    this.files = this.getTreeNodesData()
     this.trackService.getAllTracks().then(tracks => {
       this.tracks = tracks;
       console.log("Received Tracks")
       console.log(tracks)
-    }).then(()=>{
-      setTimeout(()=>{
-        console.log("Showing single track")
-        this.mapTrackMode = this.tracks[0]
-        //this.mapTrackMode.set(this.tracks[0])
-        setTimeout(() =>{
-          console.log("subscribing to latest track")
-          this.mapTrackMode = "latest"
-          //this.mapTrackMode.set("latest")
-        },20000)
-      }, 20000)
+    }).then(() => {
+      this.files = this.getTreeNodesData()
     })
   }
 
   getTreeNodesData() {
+    const trackNodes: Array<TreeNode> = this.tracks.map((track, index) => {
+      return {
+        key: '0-' + index + 1,
+        label: track.startTimestamp.toString(),
+        data: track,
+        icon: 'pi pi-fw pi-cog',
+      }
+    })
+    const latestNode: Array<TreeNode> = [{
+      key: '0',
+      label: 'Current track',
+      data: 'latest',
+      icon: 'pi pi-fw pi-cog',
+    }]
+    const nodes = latestNode.concat(trackNodes)
     return [
+
       {
-        key: '0',
+        key: '1',
         label: 'Tracks',
-        data: 'Documents Folder',
+        data: 'Tracks Folder',
         icon: 'pi pi-fw pi-folder-plus',
-        children: [
-          {
-            key: '0-0',
-            label: 'Work',
-            data: 'Work Folder',
-            icon: 'pi pi-fw pi-cog',
-            children: [
-              { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-              { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
-            ]
-          },
-          {
-            key: '0-1',
-            label: 'Home',
-            data: 'Home Folder',
-            icon: 'pi pi-fw pi-home',
-            children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
-          }
-        ]
+        children: nodes
       },
 
     ];
