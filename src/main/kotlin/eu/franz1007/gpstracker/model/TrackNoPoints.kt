e1725564c48a82a6369b21477d447e34b76936e6
franz1007 <franz.dindorf@gmail.com>
2025-04-02 18:40:56 +0200

    Import works correctly

--- src/main/kotlin/eu/franz1007/gpstracker/model/TrackNoPoints.kt
+++
@@ -1,9 +0,0 @@
-package eu.franz1007.gpstracker.model
-
-import kotlinx.datetime.Instant
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TrackNoPoints(
-    val id: Long, val startTimestamp: Instant, val endTimestamp: Instant
-)
