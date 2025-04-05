6bb3cdaba3444273c4770d169078f1b6eba7ec24
franz1007 <franz.dindorf@gmail.com>
2025-04-06 00:22:26 +0200

    added metadata to tracks

--- src/app/tracker/map/trackNoPoints.ts
+++ src/app/tracker/map/trackNoPoints.ts
@@ -9,3 +9,17 @@ export class TrackNoPoints {
         this.endTimestamp = endTimestamp;
     }
 }
+export class TrackWithMetadata {
+    id: number;
+    startTimestamp: Instant;
+    endTimestamp: Instant;
+    distanceMeters?: number;
+    averageSpeedKph?: number
+    constructor(id: number, startTimestamp: Instant, endTimestamp: Instant, distanceMeters?: number, averageSpeedKph?: number) {
+        this.id = id;
+        this.startTimestamp = startTimestamp;
+        this.endTimestamp = endTimestamp;
+        this.distanceMeters = distanceMeters;
+        this.averageSpeedKph = averageSpeedKph;
+    }
+}
