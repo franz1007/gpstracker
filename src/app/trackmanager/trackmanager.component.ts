a45e2572cf800cb40d2fae7be3c1b5fe173435f7
franz1007 <franz.dindorf@gmai.com>
2025-04-05 02:44:24 +0200

    trackmanager

--- src/app/trackmanager/trackmanager.component.ts
+++ src/app/trackmanager/trackmanager.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, linkedSignal, resource, Signal, WritableSignal } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { TrackNoPoints } from '../tracker/map/trackNoPoints';
+import { TrackService } from '../tracker/map/services/track.service';
 
 @Component({
   selector: 'app-trackmanager',
@@ -8,5 +10,23 @@ import { TableModule } from 'primeng/table';
   styleUrl: './trackmanager.component.css'
 })
 export class TrackmanagerComponent {
+  tracksResource = resource(
+    {
+      loader: ({ request, abortSignal }): Promise<Array<TrackNoPoints>> => {
+        console.log("trying to load resource")
+        const promise = this.trackService.getAllTracks(abortSignal);
+        return promise
+      },
+    }
+  ).asReadonly()
+  tracks: Array<TrackNoPoints> = new Array<TrackNoPoints>()
 
+  constructor(private trackService: TrackService) {
+    effect(() => {
+      const value = this.tracksResource.value()
+      if (value !== undefined) {
+        this.tracks = value;
+      }
+    })
+  }
 }
