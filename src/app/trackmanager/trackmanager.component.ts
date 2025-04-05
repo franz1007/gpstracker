1c04fafa73c69061edbb2081eabead5968eea0dd
franz1007 <franz.dindorf@gmail.com>
2025-04-05 02:44:11 +0200

    trackmanager

---
+++ src/app/trackmanager/trackmanager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+
+@Component({
+  selector: 'app-trackmanager',
+  imports: [TableModule],
+  templateUrl: './trackmanager.component.html',
+  styleUrl: './trackmanager.component.css'
+})
+export class TrackmanagerComponent {
+
+}
