5fc9fc0ed38fb5aa2fa56a8b30485f3d85151f0c
franz1007 <franz.dindorf@gmai.com>
2025-06-04 00:41:00 +0200

    Made startTimestamp readable and changed endTimestamp to duration in trackmanager

--- frontend/src/app/tracker/map/trackNoPoints.ts
+++ frontend/src/app/tracker/map/trackNoPoints.ts
@@ -1,4 +1,4 @@
-import { Instant } from '@js-joda/core'
+import { ChronoUnit, DateTimeFormatter, Duration, Instant, ZoneId } from '@js-joda/core'
 export class TrackNoPoints {
     id: number;
     startTimestamp: Instant;
@@ -15,15 +15,26 @@ export class TrackWithMetadata {
     id: number;
     startTimestamp: Instant;
     endTimestamp: Instant;
+    duration: Duration;
+    durationString: string;
     distanceMeters?: number;
     category: string;
     averageSpeedKph?: number;
+    startTimestampString: string;
     constructor(id: number, startTimestamp: Instant, endTimestamp: Instant, category: string, distanceMeters?: number, averageSpeedKph?: number) {
         this.id = id;
         this.startTimestamp = startTimestamp;
+        this.startTimestampString = startTimestamp.atZone(ZoneId.SYSTEM).format(DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm')) + " Uhr"
         this.endTimestamp = endTimestamp;
         this.distanceMeters = distanceMeters;
         this.averageSpeedKph = averageSpeedKph;
         this.category = category
+        this.duration = Duration.between(startTimestamp, endTimestamp)
+        this.duration.toHours()
+        const hours = this.duration.toHours()
+        const minutes = this.duration.minusHours(hours).toMinutes()
+        this.durationString = hours + ":" + minutes.toString().padStart(2, "0") + " h"
+        console.log(startTimestamp.toString())
+        console.log(this.startTimestampString)
     }
 }
