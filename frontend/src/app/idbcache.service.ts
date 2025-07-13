fdd24a7f32d280ebca22b747e7190310903c2afc
franz1007 <franz.dindorf@gmail.com>
2025-07-13 17:48:26 +0200

    Replace idb-keyval with idb

--- frontend/src/app/idbcache.service.ts
+++ frontend/src/app/idbcache.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
-import { get, set, update } from 'idb-keyval';
 import { Feature } from 'geojson';
 import { TrackMetadata } from './tracker/map/trackNoPoints';
+import { openDB, DBSchema, IDBPDatabase } from 'idb';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdbcacheService {
-  constructor() { }
+  constructor() {
+  }
 
-  public async getTrackFeature(id: string): Promise<Feature<GeoJSON.LineString> | null> {
-    return await get<Track>(id).then((value) => {
+  public async getTrackFeature(id: number): Promise<Feature<GeoJSON.LineString> | null> {
+    const db = await this.initDB()
+    return db.get("features", id).then((value) => {
       if (value !== undefined) {
-        if (value.geoJsonFeature !== undefined) {
-          console.log("Successfully retreived feature for Track " + id + " from indexeddb")
-          return value.geoJsonFeature
-        }
-        else {
-          return null
-        }
+        console.log("Successfully retreived feature for Track " + id + " from indexeddb")
+        return value.feature
+      }
+      else {
+        return null
+      }
+    }).catch(() => {
+      console.log("error getting " + id + "from idb")
+      return null
+    })
+  }
+
+  public async getMetadata(id: number): Promise<TrackMetadata | null> {
+    const db = await this.initDB()
+    return db.get("metadata", id).then((value) => {
+      if (value !== undefined) {
+        console.log("Successfully retreived metadata for Track " + id + " from indexeddb")
+        return value.metadata
       }
       return null
     }).catch(() => {
@@ -28,45 +42,42 @@ export class IdbcacheService {
     })
   }
 
-  public async getMetadata(id: string): Promise<TrackMetadata | null> {
-    return await get<Track>(id).then((value) => {
-      if (value !== undefined) {
-        if (value.metadata !== undefined) {
-          console.log("Successfully retreived metadata for Track " + id + " from indexeddb")
-          return value.metadata
-        }
-        else {
-          return null
-        }
-      }
-      return null
-    }).catch(() => {
-      console.log("error getting " + id + "from idb")
-      return null
+
+
+  public storeFeature(id: number, feature: Feature<GeoJSON.LineString>) {
+    this.initDB().then(db => {
+      db.put("features", { feature: feature }, id)
+      const transaction = db.transaction("features", "readwrite")
     })
   }
 
-  
-
-  public storeFeature(id: string, feature: Feature<GeoJSON.LineString>) {
-    update<Track>(id, (oldTrack) => {
-      const track = oldTrack !== undefined ? oldTrack : new Track()
-      track.geoJsonFeature = feature
-      return track
-
+  public storeMetadata(id: number, metadata: TrackMetadata) {
+    this.initDB().then(db => {
+      db.put("metadata", { metadata: metadata }, id)
     })
   }
 
-  public storeMetadata(id: string, metadata: TrackMetadata) {
-    update<Track>(id, (oldTrack) => {
-      const track = oldTrack !== undefined ? oldTrack : new Track()
-      track.metadata = metadata
-      return track
-    })
+  async initDB() {
+    return await openDB<MyDB>('my-db', 2, {
+      upgrade(db) {
+        db.createObjectStore("features")
+        db.createObjectStore("metadata")
+      },
+    });
   }
 }
 
-class Track {
-  metadata?: TrackMetadata;
-  geoJsonFeature?: Feature<GeoJSON.LineString>;
+interface MyDB extends DBSchema {
+  features: {
+    key: number,
+    value: {
+      feature: Feature<GeoJSON.LineString>
+    };
+  };
+  metadata: {
+    key: number,
+    value: {
+      metadata: TrackMetadata
+    };
+  };
 }
