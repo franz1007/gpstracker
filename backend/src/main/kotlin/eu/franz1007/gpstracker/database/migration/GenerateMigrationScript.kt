2e3119c554feedd5c5a02d186c7854e82c8a5256
franz1007 <franz.dindorf@gmail.com>
2025-10-03 09:06:28 +0200

    migration name generation

--- backend/src/main/kotlin/eu/franz1007/gpstracker/database/migration/GenerateMigrationScript.kt
+++ backend/src/main/kotlin/eu/franz1007/gpstracker/database/migration/GenerateMigrationScript.kt
@@ -6,6 +6,10 @@ import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.jetbrains.exposed.v1.migration.jdbc.MigrationUtils
 import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.name
+import kotlin.io.path.useDirectoryEntries
 
 const val URL = "jdbc:postgresql://localhost:5432/gpstracker"
 const val USER = "gpsuser"
@@ -28,6 +32,17 @@ fun generateMigrationScript() {
     MigrationUtils.generateMigrationScript(
         GpsPointService.GpsPositions, GpsPointService.Tracks, GpsPointService.GpsPoints,
         scriptDirectory = MIGRATIONS_DIRECTORY,
-        scriptName = "V1__Migration",
+        scriptName = getNextMigrationName(Path(MIGRATIONS_DIRECTORY)),
     )
+}
+
+fun getNextMigrationName(path: Path): String {
+    val version = path.useDirectoryEntries("V*__*.sql") { sequence ->
+        sequence.map { it.name.removePrefix("V").substringBefore("__").substringBefore(".").toInt() }.sorted()
+            .lastOrNull()?.let {
+                it + 1
+            } ?: 1
+    }
+
+    return "V${version}__Migration.sql"
 }
