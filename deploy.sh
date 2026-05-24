5d76a9565ba43a0ecedd9676e1f6c389794e9907
franz1007 <franz.dindorf@gmail.com>
2026-05-24 10:52:19 +0200

    Fix cache issues

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -u # errors on undefined variables (DEPLOY_SERVER might be undefined)
 set -x #prints commands before execution
 backend/gradlew build -p backend
 docker compose -f frontend/docker-compose.yaml run --rm npm install
-docker compose -f frontend/docker-compose.yaml run --rm npm run build
+docker compose -f frontend/docker-compose.yaml run --rm npm run build --output-hashing=all
 scp backend/gpstracker/build/libs/gpstracker-all.jar $DEPLOY_SERVER:/opt/gpstracker-backend/
 ssh $DEPLOY_SERVER systemctl --user restart gpstracker-backend
 rsync --delete -r frontend/dist/angular-leaflet-example/browser/ $DEPLOY_SERVER:/var/www/gpstracker-ng
